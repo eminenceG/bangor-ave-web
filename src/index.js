25113a62301d84315be60ce8f8b25d96b281a801
lincolnneu <hu.jiab@husky.neu.edu>
2018-07-11 17:00:19 -0700

    added bootstrap

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/font-awesome/css/font-awesome.min.css';
+
 import App from './App';
 
-
+const bootstrap = require('bootstrap');
 ReactDOM.render(<App />, document.getElementById('root'));
 
