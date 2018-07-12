7a67808d2e80e2bc92bdac77608c0696e6918d96
lincolnneu <hu.jiab@husky.neu.edu>
2018-07-12 07:06:09 -0700

    register: enabled data collection: The input data will be recorded in local state. Once press register, a clean json info will be generated.

--- src/containers/register/register.js
+++ src/containers/register/register.js
@@ -7,17 +7,30 @@ class Register extends React.Component{
     constructor(props){
         super(props);
         this.state = {
+            user:'',
+            password:'',
+            confirmedPassword:'',
             status: 'applicant'
         };
-        this.register = this.register.bind(this);
+        this.handleRegister = this.handleRegister.bind(this);
 
     }
 
-    register(){
-        this.props.history.push('/register')
+    handleRegister(){
+        console.log(this.state);
+    }
+
+    handleChange(key,val){
+        this.setState({
+            [key]:val
+        })
     }
 
     render(){
+        let selectElem;
+        let inputElemUser;
+        let inputElemPassword;
+        let inputElemConfirmedPassword;
         return (
             <div>
                 <Logo></Logo>
@@ -28,28 +41,30 @@ class Register extends React.Component{
                         <input
                             placeholder="Username"
                             className="form-control mb-3"
-                            value={null}
-                            onChange={null}
-                            ref={null}/>
+                            value={this.state.user}
+                            onChange={()=>this.handleChange('user',inputElemUser.value)}
+                            ref={node=> inputElemUser = node}/>
                         <input
                             placeholder="Password"
+                            type="password"
                             className="form-control mb-3"
-                            value={null}
-                            onChange={null}
-                            ref={null}/>
+                            value={this.state.password}
+                            onChange={()=>this.handleChange('password',inputElemPassword.value)}
+                            ref={node=> inputElemPassword = node}/>
                         <input
                             placeholder="Confirmed Password"
+                            type="password"
                             className="form-control mb-3"
-                            value={null}
-                            onChange={null}
-                            ref={null}/>
+                            value={this.state.confirmedPassword}
+                            onChange={()=>this.handleChange('confirmedPassword',inputElemConfirmedPassword.value)}
+                            ref={node=> inputElemConfirmedPassword = node}/>
                     </div>
 
                     <select
                         className="form-control mb-3"
                         value={this.state.status}
-                        onChange={null}
-                        ref={null}>
+                        onChange={()=>this.handleChange('status',selectElem.value)}
+                        ref={node=> selectElem = node}>
                         <option disabled="disabled">Choose status</option>
                         <option value="Applicant">Applicant</option>
                         <option value="Employee">Employee</option>
@@ -58,7 +73,7 @@ class Register extends React.Component{
                     </select>
 
                     <button
-                        onClick={this.register}
+                        onClick={this.handleRegister}
                         className="btn btn-primary"
                         style={{marginRight: "5px"}}>Register</button>
                 </div>
