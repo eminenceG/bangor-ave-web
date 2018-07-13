9a54db3406cb94f3b0d95525e9a6a9317b7b489b
lincolnneu <hu.jiab@husky.neu.edu>
2018-07-13 10:53:42 -0700

    focus on job list now.

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom'
 import Login from './containers/login/login'
 import Register from './containers/register/register'
 import HRProfile from './containers/HR-profile/HR-profile'
-
+import Job from './containers/job/job'
 import AuthRoute from './components/auth-route/auth-route'
 import reducers from './reducers/reducer'
 import './config/config'
@@ -28,15 +28,11 @@ function HR(){
 }
 
 ReactDOM.render(
-    <Provider store={store}>
+
         <BrowserRouter>
-            <div>
-                <AuthRoute></AuthRoute>
-                <Route path='/hr' component={HR}></Route>
-                <Route path='/hr-profile' component={HRProfile}></Route>
-                <Route path='/login' component={Login}></Route>
-                <Route path='/register' component={Register}></Route>
+            <div className="container">
+                <Route path='/jobs' component={Job}></Route>
             </div>
         </BrowserRouter>
-    </Provider>, document.getElementById('root'));
+        , document.getElementById('root'));
 
