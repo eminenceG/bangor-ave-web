d29029ad0f5025fa6d242820d921b02900b0b262
yanjianliao <yanjianliao@ryous-MacBook-Pro.local>
2018-08-07 23:44:52 -0700

    resolve confilts and fixed bugs

--- src/index.js
+++ src/index.js
@@ -8,9 +8,7 @@ import LoginContainer from './containers/login/login'
 import RegisterContainer from './containers/register/register'
 import HRProfileContainer from './containers/HR-profile/HR-profile'
 import ApplicantProfileContainer from './containers/applicant-profile/applicant-profile'
-import Chat from './components/chat/chat'
-import ApplicantProfileContainer from './containers/applicant-profile/applicant-profile'
-import Dashboard from './components/dashboard/dashboard'
+// import Dashboard from './components/dashboard/dashboard'
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import combineReducer from './reducers/combineReducer';
@@ -21,7 +19,6 @@ import './index.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
 const bootstrap = require('bootstrap');
-
 
 let store = createStore(combineReducer, compose(
     applyMiddleware(thunk),
@@ -46,7 +43,6 @@ ReactDOM.render(
                         <Route path='/applicant-profile' component={ApplicantProfileContainer}/>
                         <Route path='/job/detail/:jobId' component={WidgetList}/>
                         <Route path='/chat/:user' component={Chat}/>
-                        <Route component={Dashboard}/>
                     </Switch>
                 </div>
             </BrowserRouter>
