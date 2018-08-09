5c9747b956f01f7f65df9eeab82cb510a6b6b0b2
lincolnneu <hu.jiab@husky.neu.edu>
2018-08-08 19:24:41 -0700

    implemented applicant list. Also added link to chat on the name.

---
+++ src/components/applicant/applicant.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import * as actions from "../../actions";
+import {Link} from 'react-router-dom';
+
+class Applicant extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            data:[]
+        }
+    }
+
+    componentDidMount(){
+        this.props.getUserList('HR');
+    }
+
+    render(){
+        return (
+            <div className="container">
+                <h2>Applicant front page</h2>
+                {this.props.chatUser.userList?this.props.chatUser.userList.map(v=>(
+                    v.avatar?
+                        <div className="card"  key={v.user}>
+                            <div className="row">
+                                <div className="col-4">
+                                    <img className="card-img-top" style={{width: '200px'}} src={require(`../img/${v.avatar}.png`)} alt="Card image cap"/>
+                                </div>
+                                <div className="col-8">
+                                    <div className="card-body">
+                                        <Link to={`/chat/${v._id}`}><h5 className="card-title">{v.user}</h5></Link>
+                                        <p className="card-text">{v.title}</p>
+                                        <div className="card-text">{v.posDesc?v.posDesc.split('\n').map(v=>(<div key={v}>{v}</div>)):null}</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>:null
+                )):null}
+            </div>
+        )
+
+    }
+
+}
+
+const stateToPropertiesMapper = (state) =>(
+    state
+)
+
+const dispatcherToPropsMapper = dispatch =>({
+    getUserList: (userInfo) => actions.getUserList(dispatch, userInfo)
+
+})
+
+
+
+const ApplicantContainer = connect(stateToPropertiesMapper,dispatcherToPropsMapper)(Applicant)
+
+export default ApplicantContainer;
