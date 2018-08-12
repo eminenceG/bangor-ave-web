cae6874f18c2527f0826450d9d7fa042314598b4
lincolnneu <hu.jiab@husky.neu.edu>
2018-08-12 10:19:19 -0700

    /me: fixed bug that would cause crash after refreshing. Enhanced HR-profile

--- src/components/user/user.js
+++ src/components/user/user.js
@@ -11,8 +11,8 @@ import {
   } from "react-bootstrap";
 
 import { Link } from 'react-router-dom';
-
-
+import AuthRouteContainer from '../../components/auth-route/auth-route'
+import { Redirect } from 'react-router-dom';
 export class User extends Component {
 
   constructor(props) {
@@ -31,44 +31,48 @@ export class User extends Component {
     
   }
   render() {
-    console.log(this.props);
     return (
       <div className="card container">
-        <div className="content">
-          <div className="author">
-            <center>
-              <img
-                className="avatar border-gray"
-                style={{width: '180px'}}
-                src={require(`../img/${this.props.userReducer.avatar}.png`)}
-                alt="..."
-              />
-              <h4 className="title">
-                {this.props.userReducer.user}
-                <br />
-                <small>{this.props.userReducer.title}</small>
-              </h4>
-              <p className="description text-center">{this.props.userReducer.desc}</p>
-              <Link
-                to={'/' + this.props.userReducer.status + '-profile'}>
-                Edit My Profile
-              </Link>
-            </center>
-          </div>
-        </div>
+          <AuthRouteContainer/>
+          {this.props.userReducer.redirectTo&&this.props.userReducer.redirectTo=='/login'? <Redirect to = {this.props.userReducer.redirectTo}/>:null}
+          {this.props.userReducer.avatar?<div className="content">
+              <div className="author">
+                  <center>
+                      <img
+                          className="avatar border-gray"
+                          style={{width: '180px'}}
+                          src={require(`../img/${this.props.userReducer.avatar}.png`)}
+                          alt="..."
+                      />
+                      <h4 className="title">
+                          {this.props.userReducer.user}
+                          <br />
+                          <small>{this.props.userReducer.status} from {this.props.userReducer.company}</small>
+                          <br />
+                          <small>Hiring position: {this.props.userReducer.title}</small>
+                          <br />
+                      </h4>
+                      <p className="description text-center">{this.props.userReducer.desc}</p>
+                      <Link
+                          to={'/' + this.props.userReducer.status + '-profile'}>
+                          Edit My Profile
+                      </Link>
+                  </center>
+              </div>
+          </div>:null}
         <hr />
 
         <div className="text-center">
           <div>
-            <Button simple>
-              <i className="fa fa-facebook-square" />
-            </Button>
-            <Button simple>
-              <i className="fa fa-twitter" />
-            </Button>
-            <Button simple>
-              <i className="fa fa-google-plus-square" />
-            </Button>
+            {/*<Button simple>*/}
+              {/*<i className="fa fa-facebook-square" />*/}
+            {/*</Button>*/}
+            {/*<Button simple>*/}
+              {/*<i className="fa fa-twitter" />*/}
+            {/*</Button>*/}
+            {/*<Button simple>*/}
+              {/*<i className="fa fa-google-plus-square" />*/}
+            {/*</Button>*/}
           </div>
         </div>
       </div>
